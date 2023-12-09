2f08d8114db633d536ed5b56df1f8f64313aed93
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2023-12-09 14:41:00 -0800

    Initialize Kotlin Multiplatform Mobile project

---
+++ shared/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    id("app.cash.sqldelight") version "2.0.1"
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.langley.exercisestattracker"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+}
