7a6b7fc87100f3402c903940d1a73ff45351949d
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2023-12-13 15:57:22 -0800

    Implement basic list screen for exercise library.

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/ExerciseLibraryScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/ExerciseLibraryScreen.kt
@@ -1,17 +1,26 @@
 package com.langley.exercisestattracker.exerciseLibrary.presentation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.exerciseLibrary.domain.ExerciseDefinition
+import com.langley.exercisestattracker.exerciseLibrary.presentation.components.ExerciseDefinitionListItem
 
 @Composable
 fun ExerciseLibraryScreen(
@@ -33,5 +42,31 @@ fun ExerciseLibraryScreen(
                 )
             }
         }
-    ){  }
+    ){
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp),
+
+        ){
+            item {
+                Text(text = "Exercise Library",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    fontWeight = FontWeight.Bold
+                    )
+            }
+
+            items(state.exerciseDefinitions){ exerciseDefinition: ExerciseDefinition ->
+                ExerciseDefinitionListItem(
+                    exerciseDefinition,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            onEvent(ExerciseLibraryEvent.ExerciseDefinitionSelected(exerciseDefinition))
+                        }
+                    )
+            }
+        }
+    }
 }
