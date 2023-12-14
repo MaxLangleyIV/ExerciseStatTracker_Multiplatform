85422f160b269a5f9807b94116fb1173e2cfd284
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2023-12-13 17:08:28 -0800

    Add empty main application composable.

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/App.kt
@@ -0,0 +1,12 @@
+package com.langley.exercisestattracker
+
+import androidx.compose.runtime.Composable
+import com.langley.exercisestattracker.di.AppModule
+
+@Composable
+fun App(
+    isDarkTheme: Boolean,
+    appModule: AppModule
+){
+    TODO()
+}
