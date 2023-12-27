6cc5e865eaefe1661885577a53bf2a09070f82f2
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2023-12-27 12:27:20 -0800

    Implement simple LazyVerticalGrid for library.

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/ExerciseLibraryScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/ExerciseLibraryScreen.kt
@@ -1,25 +1,33 @@
 package com.langley.exercisestattracker.exerciseLibrary.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.exerciseLibrary.domain.ExerciseDefinition
 import com.langley.exercisestattracker.exerciseLibrary.presentation.components.ExerciseDefinitionListItem
+import com.langley.exercisestattracker.ui.theme.DarkColors
 
 @Composable
 fun ExerciseLibraryScreen(
@@ -42,32 +50,27 @@ fun ExerciseLibraryScreen(
             }
         }
     ){
-        LazyColumn(
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp),
             modifier = Modifier.fillMaxSize()
-                .padding(16.dp),
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background),
             contentPadding = PaddingValues(vertical = 16.dp),
+            content = {
+                items(state.exerciseDefinitions){ exerciseDefinition: ExerciseDefinition ->
+                    ExerciseDefinitionListItem(
+                        exerciseDefinition,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .background(MaterialTheme.colorScheme.secondary)
+                            .clickable {
+                                onEvent(ExerciseLibraryEvent.ExerciseDefinitionSelected(exerciseDefinition))
+                            },
+                    )
+                }
+            }
             //horizontalAlignment = Alignment.CenterHorizontally
 
-        ){
-            item {
-                Text(text = "Exercise Library",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    fontWeight = FontWeight.Bold
-                    )
-            }
-
-            items(state.exerciseDefinitions){ exerciseDefinition: ExerciseDefinition ->
-                ExerciseDefinitionListItem(
-                    exerciseDefinition,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            onEvent(ExerciseLibraryEvent.ExerciseDefinitionSelected(exerciseDefinition))
-                        },
-                    )
-            }
-        }
+        )
     }
 }
--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/components/ExerciseDefinitionListItem.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/components/ExerciseDefinitionListItem.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.exerciseLibrary.domain.ExerciseDefinition
 
@@ -25,7 +28,8 @@ fun ExerciseDefinitionListItem(
         modifier = modifier.wrapContentSize()
     ){
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize()
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             Row(
@@ -33,7 +37,8 @@ fun ExerciseDefinitionListItem(
                 verticalAlignment = Alignment.CenterVertically
             ){
                 Text(
-                    text = exerciseDefinition.exerciseName
+                    text = exerciseDefinition.exerciseName,
+                    color = MaterialTheme.colorScheme.onSecondary
                 )
             }
             Spacer(modifier = Modifier.height(8.dp))
@@ -43,7 +48,8 @@ fun ExerciseDefinitionListItem(
                 verticalAlignment = Alignment.CenterVertically
             ){
                 Text(
-                    text = exerciseDefinition.description
+                    text = exerciseDefinition.description,
+                    color = MaterialTheme.colorScheme.onSecondary
                 )
             }
         }
