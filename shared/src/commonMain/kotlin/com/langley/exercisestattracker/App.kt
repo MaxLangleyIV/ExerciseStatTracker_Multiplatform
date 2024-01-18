671c4172e089f8fd18f10380e704f04dec4b386f
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-01-18 15:15:08 -0800

    Add converter to definition dummy data class.

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/App.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/App.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.langley.exercisestattracker.core.presentation.ExerciseStatTrackerTheme
 import com.langley.exercisestattracker.di.AppModule
-import com.langley.exercisestattracker.exerciseLibrary.data.exerciseDefinitionDummyData
+import com.langley.exercisestattracker.exerciseLibrary.data.ExerciseDefinitionDummyData
 import com.langley.exercisestattracker.exerciseLibrary.presentation.ExerciseLibraryScreen
 import com.langley.exercisestattracker.exerciseLibrary.presentation.ExerciseLibraryState
 import com.langley.exercisestattracker.exerciseLibrary.presentation.ExerciseLibraryViewModel
@@ -35,7 +35,7 @@ fun App(
 
         val state by exerciseLibraryViewModel.state.collectAsState(ExerciseLibraryState())
 
-        val tempExerciseList = exerciseDefinitionDummyData
+        val tempExerciseList = ExerciseDefinitionDummyData().dummyDefinitionData
 
 //        for (exercise in tempExerciseList){
 //            val exerciseDefinition = ExerciseDefinition(
