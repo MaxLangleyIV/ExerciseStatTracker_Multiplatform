20b0095325755d78a1d80e7dbad4127533d74e36
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-01-31 19:27:25 -0800

    Create ExerciseSchedule.kt

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/domain/ExerciseSchedule.kt
@@ -0,0 +1,9 @@
+package com.langley.exercisestattracker.exerciseLibrary.domain
+
+data class ExerciseSchedule(
+    val exerciseScheduleId: Long?,
+    val exerciseScheduleName: String,
+    val exerciseRoutineCSV: String,
+    val dateCreated:Long?,
+    val isFavorite: Long?
+)
