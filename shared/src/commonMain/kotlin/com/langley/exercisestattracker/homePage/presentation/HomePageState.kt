dff62dd351affa958e2510ec9be5c223492cc880
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-07 20:15:28 -0800

    Rename HomePageState to HomeScreenState.

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/homePage/presentation/HomePageState.kt
+++
@@ -1,5 +0,0 @@
-package com.langley.exercisestattracker.homePage.presentation
-
-data class HomePageState(
-    val temp: Int // TODO() Implement state.
-)
---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/homePage/presentation/HomeScreenState.kt
@@ -0,0 +1,5 @@
+package com.langley.exercisestattracker.homePage.presentation
+
+data class HomeScreenState(
+    val temp: Int = 0 // TODO() Implement state.
+)
