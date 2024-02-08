004d99e83b38b754216870148da65984c898f218
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-08 11:02:22 -0800

    Create RecordsEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/homePage/presentation/HomeScreenState.kt
+++
@@ -1,5 +0,0 @@
-package com.langley.exercisestattracker.homePage.presentation
-
-data class HomeScreenState(
-    val temp: Int = 0 // TODO() Implement state.
-)
---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/records/RecordsEvent.kt
@@ -0,0 +1,41 @@
+package com.langley.exercisestattracker.records
+
+import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
+import com.langley.exercisestattracker.library.ExerciseLibraryFilterType
+
+sealed interface RecordsEvent {
+
+    // Main Library View Events
+    data class RecordSelected(val exerciseRecord: ExerciseRecord): RecordsEvent
+    data class OnSearchStringChanged(val value: String) : RecordsEvent
+    data class SetCurrentFilterType(val filterType: RecordsFilterType): RecordsEvent
+    data object ToggleIsSearchDropdownOpen: RecordsEvent
+    data object ClearFilterType : RecordsEvent
+
+
+    // Details View Events
+    data class EditRecord(val exerciseDefinition: ExerciseDefinition) : RecordsEvent
+    data object CloseDetailsView : RecordsEvent
+
+
+    // Edit View Events
+    data class OnNameChanged(val value: String) : RecordsEvent
+    data class OnBodyRegionChanged(val value: String) : RecordsEvent
+    data class OnTargetMusclesChanged(val value: String) : RecordsEvent
+    data class OnDescriptionChanged(val value: String) : RecordsEvent
+    data object CloseEditRecordView : RecordsEvent
+    data object SaveOrUpdateRecord : RecordsEvent
+    data object DeleteRecord : RecordsEvent
+
+
+
+
+
+
+
+
+
+
+
+}
