1ae713ebb276705289a6ba53545d264ec72fddc2
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-08 11:00:32 -0800

    Refactory library events.

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/LibraryEvent.kt
@@ -0,0 +1,37 @@
+package com.langley.exercisestattracker.library
+
+import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+
+sealed interface LibraryEvent {
+
+    // Main Library View Events
+    data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition): LibraryEvent
+    data class OnSearchStringChanged(val value: String) : LibraryEvent
+    data class SetCurrentFilterType(val filterType: ExerciseLibraryFilterType): LibraryEvent
+    data object ClearFilterType : LibraryEvent
+    data object ToggleIsSearchDropdownOpen: LibraryEvent
+
+
+    // Details View Events
+    data class EditDefinition(val exerciseDefinition: ExerciseDefinition) : LibraryEvent
+    data object CloseDetailsView : LibraryEvent
+
+
+    // Edit / Add View Events
+    data class OnNameChanged(val value: String) : LibraryEvent
+    data class OnBodyRegionChanged(val value: String) : LibraryEvent
+    data class OnTargetMusclesChanged(val value: String) : LibraryEvent
+    data class OnDescriptionChanged(val value: String) : LibraryEvent
+    data object SaveOrUpdateDef : LibraryEvent
+    data object CloseEditDefView : LibraryEvent
+    data object AddNewDefClicked : LibraryEvent
+    data object CloseAddDefClicked : LibraryEvent
+    data object DeleteDefinition : LibraryEvent
+
+
+    // For initializing dummy data.
+    data class SaveDefinition(val exerciseDefinition: ExerciseDefinition): LibraryEvent
+
+
+
+}
