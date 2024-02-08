817aa99a6475c6fe3fb39ab1a312bb2a716588e3
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-08 11:02:30 -0800

    Create RecordsViewModel.kt

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/records/RecordsViewModel.kt
@@ -0,0 +1,102 @@
+package com.langley.exercisestattracker.records
+
+import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
+import com.langley.exercisestattracker.library.ExerciseLibraryFilterType
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+class RecordsViewModel(
+
+    private val exerciseAppDataSource: ExerciseAppDataSource,
+    initialState: RecordsState = RecordsState()
+
+    ): ViewModel() {
+
+    private val _state = MutableStateFlow(initialState)
+
+    val state = combine(
+        _state,
+        exerciseAppDataSource.getRecords(),
+    ){
+            state, records ->
+
+        updateRecordsState(state, records)
+
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), RecordsState())
+
+    private fun updateRecordsState(
+        state: RecordsState,
+        records: List<ExerciseRecord>
+    ):RecordsState {
+
+        return state.copy(
+            exerciseRecords = filterRecords(
+                records = records,
+                filterType = state.searchFilterType,
+                searchString = state.searchString
+            )
+        )
+
+    }
+
+    private fun filterRecords(
+        records: List<ExerciseRecord>,
+        filterType: RecordsFilterType?,
+        searchString: String = ""
+    ): List<ExerciseRecord> {
+        var filteredRecords: List<ExerciseRecord> =
+            when (filterType) {
+
+                is RecordsFilterType.Barbell -> {
+                    records.filter {
+                        it.exerciseName.contains(
+                            "barbell",
+                            true
+                        )
+                    }
+                }
+                is RecordsFilterType.Calisthenic -> {
+                    records.filter {
+                        it.exerciseName.contains(
+                            "body",
+                            true)
+                    }
+                }
+                is RecordsFilterType.Dumbbell -> {
+                    records.filter {
+                        it.exerciseName.contains(
+                            "dumbbell",
+                            true)
+                    }
+                }
+                is RecordsFilterType.LowerBody -> {
+                    records.filter {
+                        it.exerciseName.contains(
+                            "leg",
+                            true)
+                    }
+                }
+                is RecordsFilterType.UpperBody -> {
+                    records.filter {
+                        it.exerciseName.contains(
+                            "barbell",
+                            true)
+                    }
+                }
+                null -> {
+                    records                }
+            }
+
+        if (searchString.isNotBlank()){
+            filteredRecords = filteredRecords.filter {
+                it.exerciseName.contains(searchString, true)
+            }
+        }
+
+        return filteredRecords
+    }
+}
