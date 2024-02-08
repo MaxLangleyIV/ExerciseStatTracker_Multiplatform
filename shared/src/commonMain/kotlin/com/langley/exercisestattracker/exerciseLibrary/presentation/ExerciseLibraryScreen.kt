d6af76788f29fa7db2f3b49e61d5f58fb24283d3
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-07 20:16:10 -0800

    Update ExerciseLibraryScreen.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/ExerciseLibraryScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/exerciseLibrary/presentation/ExerciseLibraryScreen.kt
@@ -3,7 +3,6 @@ package com.langley.exercisestattracker.exerciseLibrary.presentation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,27 +10,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.exerciseLibrary.domain.ExerciseDefinition
 import com.langley.exercisestattracker.exerciseLibrary.presentation.components.AddNewExerciseDefView
@@ -39,15 +31,18 @@ import com.langley.exercisestattracker.exerciseLibrary.presentation.components.E
 import com.langley.exercisestattracker.exerciseLibrary.presentation.components.ExerciseDefDetailsView
 import com.langley.exercisestattracker.exerciseLibrary.presentation.components.ExerciseDefinitionListItem
 import com.langley.exercisestattracker.exerciseLibrary.presentation.components.ExerciseLibraryTopBar
+import com.langley.exercisestattracker.navigation.ExerciseAppNavController
 
 @Composable
 fun ExerciseLibraryScreen(
+    modifier: Modifier = Modifier,
     state: ExerciseLibraryState,
     newExerciseDefinition: ExerciseDefinition?,
     onEvent: (ExerciseLibraryEvent) -> Unit,
     focusRequester: FocusRequester,
     focusManager: FocusManager,
-    interactionSource: MutableInteractionSource
+    interactionSource: MutableInteractionSource,
+    navController: ExerciseAppNavController
 ) {
     Scaffold(
         modifier = Modifier,
