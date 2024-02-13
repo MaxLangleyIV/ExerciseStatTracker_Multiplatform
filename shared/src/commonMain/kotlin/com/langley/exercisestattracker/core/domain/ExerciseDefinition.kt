ad78638eda08e571cf07a8f75d87bddabf659b55
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-12 18:33:32 -0800

    Update ExerciseDefinition.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseDefinition.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseDefinition.kt
@@ -6,6 +6,9 @@ data class ExerciseDefinition(
     val bodyRegion: String,
     val targetMuscles: String,
     val description: String,
+    val isCalisthenic: Long,
+    val isTimed: Long,
+    val duration: Long,
     val isFavorite: Long,
     val dateCreated: Long?
 )
