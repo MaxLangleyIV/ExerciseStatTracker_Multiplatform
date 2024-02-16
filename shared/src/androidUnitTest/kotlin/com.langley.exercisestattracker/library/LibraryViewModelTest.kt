399da19c071008ccdae2a9b76445a5b508d32d8f
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-15 19:22:08 -0800

    Update LibraryViewModelTest.kt

--- shared/src/androidUnitTest/kotlin/com.langley.exercisestattracker/library/LibraryViewModelTest.kt
+++ shared/src/androidUnitTest/kotlin/com.langley.exercisestattracker/library/LibraryViewModelTest.kt
@@ -64,6 +64,13 @@ class LibraryViewModelTest {
             bodyRegion = "test",
             targetMuscles = "test",
             description = "Test",
+            isWeighted = 1,
+            isCardio = 0,
+            isCalisthenic = 0,
+            isTimed = 0,
+            defaultDuration = 0,
+            hasDistance = 0,
+            defaultDistance = 0,
             isFavorite = 0,
             dateCreated = null
         )
