a02d125ab8eb1f31e0eb8a8bdf735b6bf2070475
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-16 20:29:24 -0800

    Update AddNewExerciseDefView.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/components/AddNewExerciseDefView.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/components/AddNewExerciseDefView.kt
@@ -1,7 +1,11 @@
 package com.langley.exercisestattracker.library.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,19 +13,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,15 +47,23 @@ import com.langley.exercisestattracker.library.LibraryState
 fun AddNewExerciseDefView(
     state: LibraryState,
     isVisible: Boolean,
-    newExerciseDefinition: ExerciseDefinition?,
-    onEvent: (LibraryEvent) -> Unit
+    newExerciseDefinition: ExerciseDefinition = ExerciseDefinition(),
+    onEvent: (LibraryEvent) -> Unit,
+    focusRequester: FocusRequester,
+    focusManager: FocusManager,
+    interactionSource: MutableInteractionSource
 ){
     BasicBottomSheet(
         visible = isVisible,
         modifier = Modifier.fillMaxSize()
+            .clickable(
+                indication = null,
+                interactionSource = interactionSource
+            ) { focusManager.clearFocus() }
     )
     {
 
+        // Top Row
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
@@ -62,6 +81,7 @@ fun AddNewExerciseDefView(
             }
         }
 
+        // Title Row
         Row(
             modifier = Modifier.fillMaxWidth(),
         ){
@@ -84,6 +104,7 @@ fun AddNewExerciseDefView(
         {
             Spacer(Modifier.height(16.dp))
 
+            // Name Input
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             )
@@ -114,6 +135,7 @@ fun AddNewExerciseDefView(
                 Modifier.height(16.dp)
             )
 
+            // Body Region Row
             Row(
                 modifier = Modifier.fillMaxWidth()
                     .clip(
@@ -152,6 +174,7 @@ fun AddNewExerciseDefView(
 
             Spacer(Modifier.height(16.dp))
 
+            // Target Muscles Row
             Row(
                 modifier = Modifier.fillMaxWidth()
                     .clip(
@@ -177,7 +200,7 @@ fun AddNewExerciseDefView(
                 Column()
                 {
                     ErrorDisplayingTextField(
-                        value = "${newExerciseDefinition?.targetMuscles}",
+                        value = newExerciseDefinition.targetMuscles,
                         placeholder = "Target Muscles",
                         error = state.exerciseTargetMusclesError,
                         onValueChanged = {
@@ -189,8 +212,55 @@ fun AddNewExerciseDefView(
 
             Spacer(Modifier.height(16.dp))
 
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(16.dp)
+                    )
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
+                    .padding(8.dp)
+            ){
+
+                Text( text = "Exercise Type:" )
+                Spacer(Modifier.height(8.dp))
+
+                Row {
+
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .clip(
+                                RoundedCornerShape(16.dp)
+                            )
+                            .background(MaterialTheme.colorScheme.tertiaryContainer)
+                            .clickable { onEvent(LibraryEvent.ToggleIsWeighted) }
+                            .border(
+                                width = 2.dp,
+
+                                color = if (newExerciseDefinition.isWeighted) {
+                                    MaterialTheme.colorScheme.outline
+                                } else Color.Transparent,
+
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .padding(4.dp)
+
+                    ){
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.onTertiaryContainer,
+                            text = "Weight Training"
+                        )
+                    }
+                }
+            }
+
+            Spacer(Modifier.height(16.dp))
+
             OutlinedTextField(
-                value = "${newExerciseDefinition?.description}",
+                value = newExerciseDefinition.description,
                 onValueChange = {
                     onEvent(LibraryEvent.OnDescriptionChanged(it))
                 },
