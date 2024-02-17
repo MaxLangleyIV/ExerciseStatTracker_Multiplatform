65daf9868ce8671f5dd7741b8f1d9633ede04414
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-16 20:29:26 -0800

    Update LibraryEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/LibraryEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/LibraryEvent.kt
@@ -14,15 +14,16 @@ sealed interface LibraryEvent {
 
     // Details View Events
     data class EditDefinition(val exerciseDefinition: ExerciseDefinition) : LibraryEvent
+    data class ToggleIsFavorite(val selectedExerciseDefinition: ExerciseDefinition): LibraryEvent
     data object CloseDetailsView : LibraryEvent
 
 
     // Edit / Add View Events
-    data class ToggleIsFavorite(val selectedExerciseDefinition: ExerciseDefinition): LibraryEvent
     data class OnNameChanged(val value: String) : LibraryEvent
     data class OnBodyRegionChanged(val value: String) : LibraryEvent
     data class OnTargetMusclesChanged(val value: String) : LibraryEvent
     data class OnDescriptionChanged(val value: String) : LibraryEvent
+    data object ToggleIsWeighted : LibraryEvent
     data object SaveOrUpdateDef : LibraryEvent
     data object CloseEditDefView : LibraryEvent
     data object AddNewDefClicked : LibraryEvent
@@ -32,6 +33,7 @@ sealed interface LibraryEvent {
 
     // For initializing dummy data.
     data class SaveDefinition(val exerciseDefinition: ExerciseDefinition): LibraryEvent
+
 
 
 }
