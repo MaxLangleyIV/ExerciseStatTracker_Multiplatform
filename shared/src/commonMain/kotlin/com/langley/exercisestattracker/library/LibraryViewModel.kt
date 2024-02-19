58f7499b0646a10654aefdc74a07cf3835ccf24b
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-18 16:23:37 -0800

    Update LibraryViewModel.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/LibraryViewModel.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/LibraryViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.setValue
 import com.langley.exercisestattracker.core.domain.ExerciseDefinition
 import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
 import com.langley.exercisestattracker.core.domain.ExerciseDefinitionValidator
+import com.langley.exercisestattracker.library.utils.filterDefinitionLibrary
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -53,78 +54,6 @@ class LibraryViewModel(
 
     }
 
-    private fun filterDefinitionLibrary(
-
-        definitionLibrary: List<ExerciseDefinition>,
-        filterType: ExerciseLibraryFilterType?,
-        searchString: String = ""
-
-    ):List<ExerciseDefinition> {
-
-        var filteredLibrary: List<ExerciseDefinition> =
-            when (filterType) {
-
-                is ExerciseLibraryFilterType.Barbell -> {
-                    definitionLibrary.filter {
-                        it.exerciseName.contains(
-                            "barbell",
-                            true
-                        )
-                    }
-                }
-                is ExerciseLibraryFilterType.Calisthenic -> {
-                    definitionLibrary.filter {
-                        it.isCalisthenic
-                    }
-                }
-                is ExerciseLibraryFilterType.Dumbbell -> {
-                    definitionLibrary.filter {
-                        it.exerciseName.contains(
-                            "dumbbell",
-                            true)
-                    }
-                }
-                is ExerciseLibraryFilterType.Favorite -> {
-                    definitionLibrary.filter {
-                        it.isFavorite
-                    }
-                }
-                is ExerciseLibraryFilterType.LowerBody -> {
-                    definitionLibrary.filter {
-                        it.exerciseName.contains(
-                            "leg",
-                            true)
-                    }
-                }
-                is ExerciseLibraryFilterType.UpperBody -> {
-                 definitionLibrary.filter {
-                    it.exerciseName.contains(
-                        "barbell",
-                        true)
-                    }
-                }
-
-                is ExerciseLibraryFilterType.Cardio -> {
-                    definitionLibrary.filter {
-                        it.isCardio
-                    }
-                }
-
-                null -> {
-                    definitionLibrary
-                }
-
-
-            }
-
-        if (searchString.isNotBlank()){
-            filteredLibrary = filteredLibrary.filter {
-                it.exerciseName.contains(searchString, true)
-            }
-        }
-
-        return filteredLibrary
-    }
 
     fun onEvent(event: LibraryEvent) {
         when (event) {
