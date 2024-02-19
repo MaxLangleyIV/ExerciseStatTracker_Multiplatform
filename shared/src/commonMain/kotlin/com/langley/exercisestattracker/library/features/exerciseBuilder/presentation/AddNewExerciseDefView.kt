b883aff3c06e994ef821d0b287245deff99a5cf4
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-18 16:23:28 -0800

    Create AddNewExerciseDefView.kt

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/library/features/exerciseBuilder/presentation/AddNewExerciseDefView.kt
@@ -0,0 +1,224 @@
+package com.langley.exercisestattracker.library.features.exerciseBuilder.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.langley.exercisestattracker.core.presentation.composables.BasicBottomSheet
+import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.di.AppModule
+import com.langley.exercisestattracker.library.LibraryEvent
+import com.langley.exercisestattracker.library.LibraryState
+import com.langley.exercisestattracker.library.LibraryViewModel
+import com.langley.exercisestattracker.library.features.exerciseBuilder.ExerciseBuilderViewModel
+import com.langley.exercisestattracker.library.features.exerciseBuilder.presentation.components.CardioExerciseBuilderView
+import com.langley.exercisestattracker.library.features.exerciseBuilder.presentation.components.StrengthExerciseBuilderView
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
+
+@Composable
+fun AddNewExerciseDefView(
+    appModule: AppModule,
+    state: LibraryState,
+    isVisible: Boolean,
+    newExerciseDefinition: ExerciseDefinition = ExerciseDefinition(),
+    onEvent: (LibraryEvent) -> Unit,
+    focusRequester: FocusRequester,
+    focusManager: FocusManager,
+    interactionSource: MutableInteractionSource
+){
+    val exerciseBuilderViewModel = getViewModel(
+        key = "exerciseBuilderViewModel",
+        factory = viewModelFactory {
+            ExerciseBuilderViewModel(appModule.exerciseAppDataSource)
+        }
+    )
+
+    //Intro Column Visibility
+    val introColumnVisible = remember { mutableStateOf(true) }
+
+    // Main Exercise Builders
+    val builderVisible = remember { mutableStateOf(false) }
+    val strengthBuilderVisible = remember { mutableStateOf(false) }
+    val cardioBuilderVisible = remember { mutableStateOf(false) }
+
+    // Builder View Options
+    val upperBodySelected = remember {
+        mutableStateOf(newExerciseDefinition.bodyRegion.lowercase().contains("upper"))
+    }
+    val lowerBodySelected = remember {
+        mutableStateOf(newExerciseDefinition.bodyRegion.lowercase().contains("lower"))
+    }
+    val coreSelected = remember {
+        mutableStateOf(newExerciseDefinition.bodyRegion.lowercase().contains("core"))
+    }
+
+    // Tags Section Visibility
+    val tagsSectionVisible = remember { mutableStateOf(true) }
+
+    BasicBottomSheet(
+        visible = isVisible,
+        modifier = Modifier.fillMaxSize()
+            .clickable(
+                indication = null,
+                interactionSource = interactionSource
+            ) { focusManager.clearFocus() }
+    )
+    {
+        // Top Row
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        )
+        {
+            IconButton(
+                onClick = {
+                    onEvent(LibraryEvent.CloseAddDefClicked)
+                    introColumnVisible.value = true
+                    builderVisible.value = false
+                    strengthBuilderVisible.value = false
+                    cardioBuilderVisible.value = false
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Close,
+                    contentDescription = "Close"
+                )
+            }
+        }
+
+        // Intro Column
+        if (introColumnVisible.value){
+            Column(
+                modifier = Modifier
+            ) {
+                Text(
+                    text = "I want to define a new..."
+                )
+                Button(
+                    onClick = {
+                        introColumnVisible.value = false
+                        builderVisible.value = true
+                        strengthBuilderVisible.value = true
+                    }
+                ){
+                    Text( text = "Strength Exercise" )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Button(
+                    onClick = {
+                        introColumnVisible.value = false
+                        builderVisible.value = true
+                        cardioBuilderVisible.value = true
+                    }
+                ){
+                    Text( text = "Cardio Exercise" )
+                }
+            }
+        }
+
+        // Builder Column
+        if (builderVisible.value){
+
+            // Title Row
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ){
+                Text(
+                    text = "New Exercise:",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 26.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+            // Content Body
+            if (strengthBuilderVisible.value){
+                StrengthExerciseBuilderView(
+                    state = state,
+                    newExerciseDefinition = newExerciseDefinition,
+                    onEvent = onEvent,
+                    upperBodySelectedState = upperBodySelected,
+                    lowerBodySelectedState = lowerBodySelected,
+                    coreBodySelectedState = coreSelected
+                )
+
+            }
+
+            if (cardioBuilderVisible.value){
+
+                CardioExerciseBuilderView(
+                    state = state,
+                    newExerciseDefinition = newExerciseDefinition,
+                    onEvent = onEvent,
+                    tagsSectionVisibleState = tagsSectionVisible
+                )
+            }
+
+            // Bottom Options
+            Spacer(Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = newExerciseDefinition.description,
+                onValueChange = {
+                    onEvent(LibraryEvent.OnDescriptionChanged(it))
+                },
+                placeholder = {
+                    Text(text = "Exercise Description")
+                },
+                shape = RoundedCornerShape(20.dp)
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    onEvent(LibraryEvent.SaveOrUpdateDef)
+                }
+            ){
+                Text(text = "Save")
+            }
+
+            Spacer(Modifier.height(4.dp))
+
+            Button(
+                onClick = {
+                    onEvent(LibraryEvent.CloseAddDefClicked)
+                }
+            ){
+                Text(text = "Cancel")
+            }
+        }
+    }
+}
+
+
+
