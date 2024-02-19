6cb2402711630d3ec5c941b292b5999635a72625
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-18 16:23:11 -0800

    Update BasicBottomSheet.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/presentation/composables/BasicBottomSheet.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/presentation/composables/BasicBottomSheet.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -40,7 +41,8 @@ fun BasicBottomSheet(
                 .background(MaterialTheme.colorScheme.surface)
                 .padding(8.dp)
                 .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.Top
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             content()
         }
