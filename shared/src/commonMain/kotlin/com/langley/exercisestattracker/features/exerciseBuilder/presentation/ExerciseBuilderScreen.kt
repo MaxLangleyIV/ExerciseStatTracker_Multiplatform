4a4c23ab81256f2ea36e9b809222f68fcabe1748
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-23 14:43:19 -0800

    Update ExerciseBuilderScreen.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/exerciseBuilder/presentation/ExerciseBuilderScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/exerciseBuilder/presentation/ExerciseBuilderScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -75,6 +76,10 @@ fun ExerciseBuilderScreen(
             (builderViewModel::onEvent)(ExerciseBuilderEvent.InitializeDefinition(selectedExercise))
             (builderViewModel::onEvent)(ExerciseBuilderEvent.DeclareAsInitialized)
         }
+    }
+
+    val selectedDefinitionState = remember(selectedExercise) {
+
     }
 
 
