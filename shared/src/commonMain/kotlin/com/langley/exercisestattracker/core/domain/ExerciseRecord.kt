e41fd29679b1bcbf4ff49fa4ee10dbd84601a218
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-23 20:27:04 -0800

    Update ExerciseRecord.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseRecord.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseRecord.kt
@@ -1,17 +1,19 @@
 package com.langley.exercisestattracker.core.domain
 
 data class ExerciseRecord(
-    val exerciseRecordId: Long?,
-    val dateCompleted: Long,
-    val exerciseName: String,
-    val weightUsed: Float,
-    val isCardio: Boolean,
-    val isCalisthenic: Boolean,
-    val duration: Float,
-    val distance: Float,
-    val repsCompleted: Int,
-    val rpe: Int,
-    val notes: String,
-    val userId: Long,
-    val currentBodyWeight: Int
+    val exerciseRecordId: Long? = null,
+    val dateCompleted: Long = 0,
+    val exerciseName: String = "",
+    val weightUsed: Float = 0F,
+    val weightIsPounds: Boolean = true,
+    val weightIsKilos: Boolean = false,
+    val isCardio: Boolean = false,
+    val isCalisthenic: Boolean = false,
+    val duration: Float = 0F,
+    val distance: Float = 0F,
+    val repsCompleted: Int = 0,
+    val rpe: Int = 0,
+    val notes: String = "",
+    val userId: Long = 0,
+    val currentBodyWeight: Int = 0
 )
