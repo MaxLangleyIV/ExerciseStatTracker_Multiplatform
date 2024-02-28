92f4bd107e461f6ff54b4b7d0ee7331277bb5195
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-28 15:20:35 -0800

    Update WorkoutViewModel.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutViewModel.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutViewModel.kt
@@ -1,4 +1,23 @@
 package com.langley.exercisestattracker.features.workout
 
-class WorkoutViewModel {
+import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
+import com.langley.exercisestattracker.features.exerciseBuilder.ExerciseBuilderState
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+
+class WorkoutViewModel(
+    private val dataSource: ExerciseAppDataSource,
+    initialState: WorkoutState = WorkoutState()
+): ViewModel() {
+    private val _state = MutableStateFlow(initialState)
+    val state = _state
+        .asStateFlow()
+        .stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000L),
+        ExerciseBuilderState()
+        )
 }
