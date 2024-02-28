8eae1235c41468eb859ea1b36e4908a5b8f5500f
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-28 15:20:29 -0800

    Update WorkoutScreen.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/WorkoutScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/WorkoutScreen.kt
@@ -1,6 +1,7 @@
 package com.langley.exercisestattracker.features.workout.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,21 +11,43 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.unit.dp
+import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
+import com.langley.exercisestattracker.features.workout.WorkoutState
+import com.langley.exercisestattracker.features.workout.WorkoutViewModel
+import com.langley.exercisestattracker.navigation.ExerciseAppNavController
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 
 @Composable
 fun WorkoutScreen(
     modifier: Modifier = Modifier
         .fillMaxSize()
-        .background(Color.Black)
-        .padding(8.dp)
+        .background(MaterialTheme.colorScheme.background)
+        .padding(8.dp),
+    dataSource: ExerciseAppDataSource,
+    focusRequester: FocusRequester,
+    focusManager: FocusManager,
+    interactionSource: MutableInteractionSource,
+    navController: ExerciseAppNavController
 ){
+    val workoutViewModel = getViewModel(
+        key = "workoutViewModel",
+        factory = viewModelFactory { WorkoutViewModel(dataSource) }
+    )
+
+    val state by workoutViewModel.state.collectAsState(WorkoutState())
+
     // Full Screen Container
     Column(
         modifier = modifier
@@ -35,7 +58,7 @@ fun WorkoutScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(16.dp))
-                .background(Color.DarkGray)
+                .background(MaterialTheme.colorScheme.surfaceVariant)
                 .weight(0.1F),
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
@@ -50,7 +73,7 @@ fun WorkoutScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(16.dp))
-                .background(Color.DarkGray)
+                .background(MaterialTheme.colorScheme.surfaceVariant)
                 .weight(0.8F),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
