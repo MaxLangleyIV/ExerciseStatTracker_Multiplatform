0fb4f629b67df299f9044509890d63611893dc32
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-02-28 15:20:05 -0800

    Update WorkoutScreenPreview.kt

--- shared/src/androidMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/WorkoutScreenPreview.kt
+++ shared/src/androidMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/WorkoutScreenPreview.kt
@@ -1,10 +1,30 @@
 package com.langley.exercisestattracker.features.workout.presentation
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
+import com.langley.exercisestattracker.navigation.ExerciseAppNavController
+import com.langley.exercisestattracker.navigation.Screen
 
 @Preview
 @Composable
 fun WorkoutScreenPreview() {
-    WorkoutScreen()
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val navController = remember { ExerciseAppNavController(Screen.Workout) }
+    val currentScreen by navController.currentScreen.collectAsState()
+//    WorkoutScreen(
+//        dataSource = ,
+//        focusRequester = focusRequester,
+//        focusManager = focusManager,
+//        interactionSource = interactionSource,
+//        navController = navController
+//    )
 }
