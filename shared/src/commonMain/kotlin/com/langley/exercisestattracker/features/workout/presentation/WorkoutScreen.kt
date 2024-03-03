a54ee25b8ca02e422c5de289d8ebc253e56ac0e8
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-02 16:35:28 -0800

    Update WorkoutScreen.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/WorkoutScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/WorkoutScreen.kt
@@ -1,6 +1,7 @@
 package com.langley.exercisestattracker.features.workout.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -16,11 +18,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
 import com.langley.exercisestattracker.features.workout.WorkoutState
@@ -39,7 +44,8 @@ fun WorkoutScreen(
     focusRequester: FocusRequester,
     focusManager: FocusManager,
     interactionSource: MutableInteractionSource,
-    navController: ExerciseAppNavController
+    navController: ExerciseAppNavController,
+    visible: Boolean = true
 ){
     val workoutViewModel = getViewModel(
         key = "workoutViewModel",
@@ -47,6 +53,8 @@ fun WorkoutScreen(
     )
 
     val state by workoutViewModel.state.collectAsState(WorkoutState())
+
+    var isSingleExerciseMode by mutableStateOf( true )
 
     // Full Screen Container
     Column(
@@ -68,21 +76,117 @@ fun WorkoutScreen(
 
         Spacer(Modifier.height(8.dp))
 
-        // Current Exercise Container
+        // Workout Content
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .clip(RoundedCornerShape(16.dp))
-                .background(MaterialTheme.colorScheme.surfaceVariant)
+                .background(MaterialTheme.colorScheme.background)
                 .weight(0.8F),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Text(text = "Exercise Container")
+
+            // Empty Workout View
+            if (state.exerciseQueue.isEmpty()){
+                Text(
+                    text = "This workout is currently empty.",
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+
+                // Buttons Column
+                Column {
+                    // Add Exercise Button
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 0.dp, max = 60.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .clickable {  }
+                            .background(MaterialTheme.colorScheme.primaryContainer)
+                            .padding(4.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1F),
+                            text = "Add an exercise.",
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+
+                    Spacer(Modifier.height(8.dp))
+
+                    // Select Routine Button
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 0.dp, max = 60.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .clickable {  }
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .padding(4.dp),
+                        horizontalArrangement = Arrangement.Center,
+                    ){
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1F),
+                            text = "Select a routine.",
+                            color = MaterialTheme.colorScheme.onSecondaryContainer,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+
+                    Spacer(Modifier.height(8.dp))
+
+                    // Cancel
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 0.dp, max = 60.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .clickable {  }
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .padding(4.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1F),
+                            text = "Cancel",
+                            color = MaterialTheme.colorScheme.onSecondaryContainer,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+
+
+            }
+            // Non-empty Workout View
+            else {
+
+                // Exercises
+                for (exercise in state.exerciseQueue){
+
+                    Column {
+                        Row {
+                            Text( text = exercise.exerciseName )
+                        }
+                    }
+
+                }
+
+            }
+
+
         }
-
-        //
-
     }
 
 }
