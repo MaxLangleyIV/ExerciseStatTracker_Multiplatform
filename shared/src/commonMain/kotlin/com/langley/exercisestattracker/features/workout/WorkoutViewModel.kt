3fb4fe8e23c754fe3e72c3d454a24826655d02a6
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-03 20:38:20 -0800

    Update WorkoutViewModel.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutViewModel.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutViewModel.kt
@@ -2,7 +2,7 @@ package com.langley.exercisestattracker.features.workout
 
 import com.langley.exercisestattracker.core.data.dummyData.ExerciseRoutineDummyData
 import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
-import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -22,24 +22,51 @@ class WorkoutViewModel(
     val state = _state
         .asStateFlow()
         .stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000L),
-        WorkoutState()
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000L),
+            initialValue = WorkoutState()
         )
+
 
     // FOR DEVELOPMENT
     init {
         viewModelScope.launch {
             val definitions = dataSource.getDefinitions().first()
             val currentRoutine = ExerciseRoutineDummyData(definitions).getRoutines()[0]
+
             val exerciseIdList = currentRoutine.exerciseCSV.split(",")
-            println(exerciseIdList)
+            val repsList = currentRoutine.repsCSV.split(",")
 
-            val currentExerciseQueue = mutableListOf<ExerciseDefinition>()
+            val currentExerciseQueue = mutableListOf<ExerciseRecord>()
 
-            for (idString in exerciseIdList){
+            for ((index,idString) in exerciseIdList.withIndex()){
                 try {
-                    currentExerciseQueue.add(definitions[idString.toInt()])
+                    val def = definitions[idString.toInt()]
+                    val reps = repsList[index].toInt()
+
+                    if (_state.value.exerciseMap[def.exerciseName] == null){
+                        _state.value.exerciseMap[def.exerciseName] =
+                            mutableListOf(
+                                ExerciseRecord(
+                                    exerciseName = def.exerciseName,
+                                    isCardio = def.isCardio,
+                                    isCalisthenic = def.isCalisthenic,
+                                    repsCompleted = reps
+                                )
+                            )
+
+                    }
+                    else {
+                        _state.value.exerciseMap[def.exerciseName]!!.add(
+                            ExerciseRecord(
+                                exerciseName = def.exerciseName,
+                                isCardio = def.isCardio,
+                                isCalisthenic = def.isCalisthenic,
+                                repsCompleted = reps
+                            )
+                        )
+                    }
+
                 }
                 catch (error : IndexOutOfBoundsException){
                     println("Index passed: $idString")
@@ -49,7 +76,7 @@ class WorkoutViewModel(
 
             _state.update { it.copy(
                 routine = currentRoutine,
-                exerciseQueue = currentExerciseQueue
+                exerciseQueue = currentExerciseQueue,
             ) }
         }
 
