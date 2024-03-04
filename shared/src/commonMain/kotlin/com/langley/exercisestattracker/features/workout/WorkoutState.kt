45caa1746f5e66f18148e766623c165fa2a66ef2
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-03 20:38:19 -0800

    Update WorkoutState.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutState.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutState.kt
@@ -1,6 +1,5 @@
 package com.langley.exercisestattracker.features.workout
 
-import com.langley.exercisestattracker.core.domain.ExerciseDefinition
 import com.langley.exercisestattracker.core.domain.ExerciseRecord
 import com.langley.exercisestattracker.core.domain.ExerciseRoutine
 
@@ -8,9 +7,11 @@ data class WorkoutState(
 
     val routine: ExerciseRoutine = ExerciseRoutine(),
 
-    val exerciseQueue: List<ExerciseDefinition> = mutableListOf(),
+    val exerciseQueue: List<ExerciseRecord> = mutableListOf(),
 
-    val completedExercises: List<ExerciseRecord> = mutableListOf()
+    val completedExercises: List<ExerciseRecord> = mutableListOf(),
+
+    val exerciseMap: MutableMap<String, MutableList<ExerciseRecord>> = mutableMapOf()
 
 
 
