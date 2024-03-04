05d53ee821e288913b9739e0ab1b852049cb1bec
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-03 20:38:15 -0800

    Update ExerciseRoutine.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseRoutine.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseRoutine.kt
@@ -3,7 +3,7 @@ package com.langley.exercisestattracker.core.domain
 data class ExerciseRoutine(
     val exerciseRoutineId: Long? = null,
     val routineName: String = "",
-    val exerciseCSV: String = "",
+    val exerciseCSV: String = "", // CSV of exercise IDs
     val repsCSV: String = "",
     val description: String = "",
     val isFavorite: Boolean = false,
