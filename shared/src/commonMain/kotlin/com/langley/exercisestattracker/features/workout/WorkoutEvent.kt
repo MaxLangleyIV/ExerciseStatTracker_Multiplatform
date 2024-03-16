fb7e4b47dd3f0c109fb0c29453835709d30eedda
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-15 20:46:53 -0700

    Update WorkoutEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutEvent.kt
@@ -6,12 +6,17 @@ import com.langley.exercisestattracker.core.domain.ExerciseRecord
 sealed interface WorkoutEvent {
     data object OpenExerciseSelector: WorkoutEvent
     data object CloseExerciseSelector: WorkoutEvent
-    data class AddRecord(val record: ExerciseRecord): WorkoutEvent
-    data class RemoveRecord(val recordName: String, val index: Int): WorkoutEvent
+    data class AddRecordToMap(val record: ExerciseRecord): WorkoutEvent
+    data class RemoveRecordFromMap(val recordName: String, val index: Int): WorkoutEvent
     data object SaveWorkout: WorkoutEvent
     data class MarkCompleted(val record: ExerciseRecord): WorkoutEvent
     data class RemoveFromCompleted(val index: Int): WorkoutEvent
     data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition) : WorkoutEvent
+    data class AddToListOfExercises(val exercises: List<ExerciseDefinition>) : WorkoutEvent
+    data class RemoveFromListOfExercises(val exercise: ExerciseDefinition) : WorkoutEvent
+    data class AddToListOfRecords(val exercises: List<ExerciseDefinition>) : WorkoutEvent
+    data class RemoveFromListOfRecords(val exercise: ExerciseDefinition) : WorkoutEvent
+
 
     data object NextExercise
 }
