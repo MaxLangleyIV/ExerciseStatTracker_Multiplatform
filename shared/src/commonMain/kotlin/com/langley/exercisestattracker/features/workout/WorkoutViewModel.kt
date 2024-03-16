7df4341b12ade949eeb31dbf858b04728f6d3df1
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-15 19:42:09 -0700

    Update WorkoutViewModel.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutViewModel.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutViewModel.kt
@@ -2,7 +2,6 @@ package com.langley.exercisestattracker.features.workout
 
 import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
 import com.langley.exercisestattracker.core.domain.ExerciseDefinition
-import com.langley.exercisestattracker.core.domain.ExerciseRecord
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -10,6 +9,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class WorkoutViewModel(
     private val dataSource: ExerciseAppDataSource,
@@ -35,89 +35,13 @@ class WorkoutViewModel(
         currentDefinitions, newDefinitions ->
 
         newDefinitions
+
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000L),
         initialValue = listOf<ExerciseDefinition>()
     )
 
-    // FOR DEVELOPMENT
-//    init {
-//        viewModelScope.launch {
-//            val definitions = dataSource.getDefinitions().first()
-//            val currentRoutine = ExerciseRoutineDummyData(definitions).getRoutines()[0]
-//
-//            val exerciseIdList = currentRoutine.exerciseCSV.split(",")
-//            val repsList = currentRoutine.repsCSV.split(",")
-//
-//            val currentExerciseQueue = mutableListOf<ExerciseRecord>()
-//
-//            val newMap = _state.value.exerciseMap.toMutableMap()
-//
-//            for ((index,idString) in exerciseIdList.withIndex()){
-//                try {
-//                    val def = definitions[idString.toInt()]
-//                    val reps = repsList[index].toInt()
-//
-//                    if (newMap[def.exerciseName] == null){
-//
-//                        newMap[def.exerciseName] =
-//                            listOf(
-//                                ExerciseRecord(
-//                                    exerciseName = def.exerciseName,
-//                                    isCardio = def.isCardio,
-//                                    isCalisthenic = def.isCalisthenic,
-//                                    repsCompleted = reps
-//                                )
-//                            )
-//
-//                    }
-//                    else {
-//                        newMap[def.exerciseName] =
-//                            newMap[def.exerciseName]!!
-//                                .toMutableList() + ExerciseRecord(
-//                                exerciseName = def.exerciseName,
-//                                isCardio = def.isCardio,
-//                                isCalisthenic = def.isCalisthenic,
-//                                repsCompleted = reps
-//                                )
-//
-//                    }
-//
-//                    _state.update { it.copy(
-//                        exerciseMap = newMap
-//                    ) }
-//
-//                }
-//                catch (error : IndexOutOfBoundsException){
-//                    println("Index passed: $idString")
-//                }
-//            }
-//
-//
-//            _state.update { it.copy(
-//                routine = currentRoutine,
-//                exerciseQueue = currentExerciseQueue,
-//            ) }
-//        }
-//    }
-
-    fun addToMap(record: ExerciseRecord?){
-        if (record == null) { return }
-
-        val newMap = _state.value.exerciseMap.toMutableMap()
-
-        if (newMap[record.exerciseName] == null){
-            newMap[record.exerciseName] = listOf(record).toMutableList()
-        }
-        else {
-            newMap[record.exerciseName] = newMap[record.exerciseName]!!.toMutableList() + record
-        }
-
-        _state.update { it.copy(
-            exerciseMap = newMap
-        ) }
-    }
 
     fun onEvent(workoutEvent: WorkoutEvent){
 
@@ -209,8 +133,36 @@ class WorkoutViewModel(
                 ) }
             }
 
-            WorkoutEvent.SaveWorkout -> {}
+            WorkoutEvent.SaveWorkout -> {
 
+                viewModelScope.launch {
+
+                    for (record in _state.value.completedExercises){
+
+                        dataSource.insertOrReplaceRecord(record)
+
+                    }
+
+                }
+
+            }
+
+            is WorkoutEvent.DefinitionSelected -> {
+
+                val mutableList = _state.value.selectedExercises.toMutableList()
+
+                if (mutableList.contains(workoutEvent.exerciseDefinition)){
+                    mutableList.remove(workoutEvent.exerciseDefinition)
+                }
+                else {
+                    mutableList.add(workoutEvent.exerciseDefinition)
+                }
+
+                _state.update { it.copy(
+                    selectedExercises = mutableList
+                ) }
+
+            }
         }
 
     }
