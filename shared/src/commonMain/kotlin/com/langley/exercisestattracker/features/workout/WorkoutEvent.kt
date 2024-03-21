cfb1db2e3da130092f2e90302b53846ee97ef25f
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-20 18:34:04 -0700

    Update WorkoutEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutEvent.kt
@@ -2,21 +2,33 @@ package com.langley.exercisestattracker.features.workout
 
 import com.langley.exercisestattracker.core.domain.ExerciseDefinition
 import com.langley.exercisestattracker.core.domain.ExerciseRecord
+import com.langley.exercisestattracker.features.library.ExerciseLibraryFilterType
 
 sealed interface WorkoutEvent {
+
+    // Main Events
     data object OpenExerciseSelector: WorkoutEvent
     data object CloseExerciseSelector: WorkoutEvent
-    data class AddRecordToMap(val record: ExerciseRecord): WorkoutEvent
-    data class RemoveRecordFromMap(val recordName: String, val index: Int): WorkoutEvent
     data object SaveWorkout: WorkoutEvent
     data class MarkCompleted(val record: ExerciseRecord): WorkoutEvent
     data class RemoveFromCompleted(val index: Int): WorkoutEvent
-    data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition) : WorkoutEvent
-    data class AddToListOfExercises(val exercises: List<ExerciseDefinition>) : WorkoutEvent
     data class RemoveFromListOfExercises(val index: Int) : WorkoutEvent
     data class AddToListOfRecords(val records: List<ExerciseRecord>) : WorkoutEvent
     data class RemoveFromListOfRecords(val index: Int) : WorkoutEvent
 
+    // Exercise Selector Events
+    data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition) : WorkoutEvent
+    data class AddToListOfExercises(val exercises: List<ExerciseDefinition>) : WorkoutEvent
+    data class OnSearchStringChanged(val string: String) : WorkoutEvent
+    data object ClearFilterType : WorkoutEvent
+
+
+    // Deprecated
+    data class AddRecordToMap(val record: ExerciseRecord): WorkoutEvent
+    data class RemoveRecordFromMap(val recordName: String, val index: Int): WorkoutEvent
+    data class SetCurrentFilterType(val favorite: ExerciseLibraryFilterType) : WorkoutEvent
+
 
     data object NextExercise
+
 }
