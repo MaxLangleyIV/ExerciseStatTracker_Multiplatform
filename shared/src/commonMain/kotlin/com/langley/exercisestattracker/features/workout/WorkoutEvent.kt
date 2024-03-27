b48a784d9fea8655f34fc9b3e444a48472498cce
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-27 14:34:30 -0700

    Update WorkoutEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutEvent.kt
@@ -12,23 +12,24 @@ sealed interface WorkoutEvent {
     data object SaveWorkout: WorkoutEvent
     data class MarkCompleted(val index: Int, val record: ExerciseRecord): WorkoutEvent
     data class MarkIncomplete(val index: Int, val record: ExerciseRecord): WorkoutEvent
-    data class RemoveFromCompleted(val index: Int, val record: ExerciseRecord): WorkoutEvent
     data class RemoveFromListOfExercises(val exercise: ExerciseDefinition) : WorkoutEvent
     data class AddToListOfRecords(val records: List<ExerciseRecord>) : WorkoutEvent
     data class RemoveFromListOfRecords(val index: Int) : WorkoutEvent
-    data class ToggleCompleted(val record: ExerciseRecord) : WorkoutEvent
+    data class UpdateRecordInList(val index: Int, val newRecord: ExerciseRecord) : WorkoutEvent
+//    data class ToggleCompleted(val record: ExerciseRecord) : WorkoutEvent
 
     // Exercise Selector Events
     data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition) : WorkoutEvent
     data class AddToListOfExercises(val exercises: List<ExerciseDefinition>) : WorkoutEvent
     data class OnSearchStringChanged(val string: String) : WorkoutEvent
+    data class SetCurrentFilterType(val filter: ExerciseLibraryFilterType) : WorkoutEvent
     data object ClearFilterType : WorkoutEvent
 
 
     // Deprecated
-    data class AddRecordToMap(val record: ExerciseRecord): WorkoutEvent
-    data class RemoveRecordFromMap(val recordName: String, val index: Int): WorkoutEvent
-    data class SetCurrentFilterType(val filter: ExerciseLibraryFilterType) : WorkoutEvent
+//    data class AddRecordToMap(val record: ExerciseRecord): WorkoutEvent
+//    data class RemoveRecordFromMap(val recordName: String, val index: Int): WorkoutEvent
+//    data class RemoveFromCompleted(val index: Int, val record: ExerciseRecord): WorkoutEvent
 
 
     data object NextExercise
