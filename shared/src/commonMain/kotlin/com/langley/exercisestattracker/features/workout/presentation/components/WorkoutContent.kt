f5ec5e9854c5ad6a84ae54dc579faabebef24caa
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-28 16:33:30 -0700

    Update WorkoutContent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WorkoutContent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WorkoutContent.kt
@@ -3,14 +3,11 @@ package com.langley.exercisestattracker.features.workout.presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.core.domain.ExerciseRecord
+import com.langley.exercisestattracker.core.presentation.composables.DividerRow
 import com.langley.exercisestattracker.features.workout.WorkoutEvent
 import com.langley.exercisestattracker.features.workout.WorkoutState
 import kotlinx.datetime.Clock
@@ -56,76 +54,15 @@ fun WorkoutContent(
                 for ((recordIndex, set) in workoutState.recordsList.withIndex()){
 
                     if (set.exerciseName == exercise.exerciseName){
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
 
-                            if (exercise.hasReps){
+                        ExerciseRecordRow(
+                            exercise = exercise,
+                            set = set,
+                            recordIndex = recordIndex,
+                            onEvent = onEvent
+                        )
 
-                                Column {
-                                    Text(
-                                        text = "Reps:",
-                                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                                    )
-                                }
-
-                                Spacer(Modifier.width(4.dp))
-
-                                Column {
-                                    Text(
-                                        text = set.repsCompleted.toString(),
-                                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                                    )
-                                }
-
-                                Spacer(Modifier.width(4.dp))
-
-                            }
-
-                            if (exercise.isWeighted){
-
-                                Column {
-                                    Text(
-                                        text = "Weight:",
-                                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                                    )
-                                }
-
-                                Spacer(Modifier.width(4.dp))
-
-                                Column {
-                                    Text(
-                                        text = set.weightUsed.toString(),
-                                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                                    )
-                                }
-
-                                Spacer(Modifier.width(4.dp))
-
-
-
-                            }
-
-                            Column {
-                                Checkbox(
-                                    onCheckedChange = {isChecked ->
-                                        if (isChecked){
-                                            onEvent(
-                                                WorkoutEvent.MarkCompleted(recordIndex, set)
-                                            )
-                                        }
-                                        else {
-                                            onEvent(
-                                                WorkoutEvent.MarkIncomplete(recordIndex, set)
-                                            )
-                                        }
-                                    },
-                                    checked = set.completed
-                                )
-                            }
-                        }
+                        DividerRow()
 
                         lastSetEntered = set
                     }
