b02d7d7ed2a3bdfaf3b5fad278eee969e7a4bbc6
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-30 15:45:24 -0700

    Update WeightTrainingRecordRow.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WeightTrainingRecordRow.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WeightTrainingRecordRow.kt
@@ -1,23 +1,24 @@
 package com.langley.exercisestattracker.features.workout.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.langley.exercisestattracker.core.domain.ExerciseDefinition
@@ -29,6 +30,7 @@ fun WeightTrainingRecordRow(
     modifier: Modifier = Modifier,
     exercise: ExerciseDefinition = ExerciseDefinition(),
     set: ExerciseRecord = ExerciseRecord(),
+    setNumber: Int = 0,
     onEvent: (WorkoutEvent) -> Unit,
     recordIndex: Int
     ){
@@ -36,7 +38,9 @@ fun WeightTrainingRecordRow(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(IntrinsicSize.Min)
+            .heightIn( min = 32.dp, max = 68.dp)
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .clip(RoundedCornerShape(8.dp))
             .border(
                 width = 4.dp,
                 color = MaterialTheme.colorScheme.outline,
@@ -45,31 +49,66 @@ fun WeightTrainingRecordRow(
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
     ) {
-
-        // Weight Used
+        //Title
         Column(
             modifier = Modifier
-                .weight(0.3F)
+                .weight(0.2F)
                 .padding(4.dp),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
             Text(
-                modifier = Modifier.weight(0.3F),
-                text = "Weight: ",
-                color = MaterialTheme.colorScheme.onSecondaryContainer,
-                textAlign = TextAlign.Start
+                text = "Set $setNumber"
             )
 
-            Spacer( Modifier.height(4.dp) )
 
-            Text(
-                modifier = Modifier.weight(0.7F),
-                text = set.weightUsed.toString(),
-                color = MaterialTheme.colorScheme.onSecondaryContainer,
-                textAlign = TextAlign.Center
-            )
+        }
+
+        Divider(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(4.dp),
+            color = MaterialTheme.colorScheme.outline
+        )
+
+        // Weight Used
+        Column(
+            modifier = Modifier
+                .weight(0.2F)
+                .padding(4.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .weight(0.3F)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.weight(0.3F),
+                    text = "Weight: ",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .weight(0.4F)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = set.weightUsed.toString(),
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    textAlign = TextAlign.Center
+                )
+            }
 
         }
 
@@ -83,16 +122,43 @@ fun WeightTrainingRecordRow(
         // Reps Performed
         Column(
             modifier = Modifier
-                .weight(0.3F)
+                .weight(0.2F)
                 .padding(4.dp),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Row {  }
-            Row {  }
+            Row(
+                modifier = Modifier
+                    .weight(0.3F)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.weight(0.3F),
+                    text = "Reps: ",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .weight(0.4F)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = set.repsCompleted.toString(),
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    textAlign = TextAlign.Center
+                )
+            }
 
         }
+
 
         Divider(
             modifier = Modifier
@@ -104,16 +170,53 @@ fun WeightTrainingRecordRow(
         // Completion Status
         Column(
             modifier = Modifier
-                .weight(0.3F)
+                .weight(0.2F)
                 .padding(4.dp),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Row {  }
-            Row {  }
+            Row(
+                modifier = Modifier
+                    .weight(0.3F)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.weight(0.3F),
+                    text = "Complete: ",
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .weight(0.3F)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    onCheckedChange = {isChecked ->
+                        if (isChecked){
+                            onEvent(
+                                WorkoutEvent.MarkCompleted(recordIndex, set)
+                            )
+                        }
+                        else {
+                            onEvent(
+                                WorkoutEvent.MarkIncomplete(recordIndex, set)
+                            )
+                        }
+                    },
+                    checked = set.completed
+                )
+            }
 
         }
+
 
     }
 
@@ -207,21 +310,7 @@ fun WeightTrainingRecordRow(
 //
 //            }
 //
-//            Checkbox(
-//                onCheckedChange = {isChecked ->
-//                    if (isChecked){
-//                        onEvent(3
-//                            WorkoutEvent.MarkCompleted(recordIndex, set)
-//                        )
-//                    }
-//                    else {
-//                        onEvent(
-//                            WorkoutEvent.MarkIncomplete(recordIndex, set)
-//                        )
-//                    }
-//                },
-//                checked = set.completed
-//            )
+
 //
 //        }
 //
