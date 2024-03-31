f783d3187cd6be54136a026b85be987fb1636f91
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-03-30 20:26:16 -0700

    Update WorkoutContentHolder.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WorkoutContentHolder.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WorkoutContentHolder.kt
@@ -2,6 +2,7 @@ package com.langley.exercisestattracker.features.workout.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -40,7 +41,7 @@ fun WorkoutContentHolder(
         else {
 
             WorkoutContent(
-                modifier = Modifier,
+                modifier = Modifier.fillMaxSize(),
                 workoutState = workoutState,
                 onEvent = onEvent
             )
