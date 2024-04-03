1d2227f0305ef8156511f838644b7b098bd11628
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-03 11:09:17 -0700

    Update App.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/App.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/App.kt
@@ -15,11 +15,19 @@ import com.langley.exercisestattracker.core.presentation.ExerciseStatTrackerThem
 import com.langley.exercisestattracker.di.AppModule
 import com.langley.exercisestattracker.features.home.HomeScreen
 import com.langley.exercisestattracker.features.home.HomeState
+import com.langley.exercisestattracker.features.library.LibraryState
+import com.langley.exercisestattracker.features.library.LibraryViewModel
 import com.langley.exercisestattracker.features.library.presentation.LibraryScreen
 import com.langley.exercisestattracker.features.records.RecordsScreen
+import com.langley.exercisestattracker.features.records.RecordsState
+import com.langley.exercisestattracker.features.records.RecordsViewModel
+import com.langley.exercisestattracker.features.workout.WorkoutState
+import com.langley.exercisestattracker.features.workout.WorkoutViewModel
 import com.langley.exercisestattracker.features.workout.presentation.WorkoutScreen
 import com.langley.exercisestattracker.navigation.ExerciseAppNavController
 import com.langley.exercisestattracker.navigation.Screen
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 
 @Composable
 fun App(
@@ -38,6 +46,45 @@ fun App(
         val navController = remember { ExerciseAppNavController() }
         val currentScreen by navController.currentScreen.collectAsState()
 
+        // View Models and States
+        val libraryViewModel = getViewModel(
+            key = "libraryViewModel",
+            factory = viewModelFactory {
+                LibraryViewModel(appModule.exerciseAppDataSource)
+            }
+        )
+
+        val libraryState by libraryViewModel.state.collectAsState(LibraryState())
+
+        val recordsViewModel = getViewModel(
+            key = "recordsViewModel",
+            factory = viewModelFactory {
+                RecordsViewModel(appModule.exerciseAppDataSource)
+            }
+        )
+        val recordsState by recordsViewModel.state.collectAsState(RecordsState())
+
+        val workoutViewModel = getViewModel(
+            key = "workoutViewModel",
+            factory = viewModelFactory { WorkoutViewModel(appModule.exerciseAppDataSource) }
+        )
+
+        val workoutState by workoutViewModel.state.collectAsState(WorkoutState())
+
+
+//    // INIT DUMMY DATA
+//    val exerciseRecordList = ExerciseDefinitionDummyData().getListOfDummyExerciseRecords()
+//    for (record in exerciseRecordList){
+//        (recordsViewModel::onEvent)(RecordsEvent.SaveRecord(record))
+//    }
+
+//    val exerciseDefDummyData = ExerciseDefinitionDummyData()
+//    val exerciseDefList = exerciseDefDummyData.definitionList
+//    // Add definitions to SQLDelight db.
+//    for (exerciseDefinition in exerciseDefList){
+//        libraryViewModel.onEvent(LibraryEvent.SaveDefinition(exerciseDefinition))
+//    }
+
 
         Surface(
             modifier = Modifier.fillMaxSize(),
@@ -49,8 +96,7 @@ fun App(
                 Screen.Home -> {
 
                     HomeScreen(
-                        // This state is a placeholder
-                        state = HomeState(),
+                        state = HomeState(), // This state is a placeholder
                         focusRequester = focusRequester,
                         focusManager = focusManager,
                         interactionSource = interactionSource,
@@ -63,6 +109,8 @@ fun App(
 
                     LibraryScreen(
                         dataSource = appModule.exerciseAppDataSource,
+                        libraryState = libraryState,
+                        onEvent = libraryViewModel::onEvent,
                         focusRequester = focusRequester,
                         focusManager = focusManager,
                         interactionSource = interactionSource,
@@ -74,7 +122,8 @@ fun App(
                 Screen.Records -> {
 
                     RecordsScreen(
-                        dataSource = appModule.exerciseAppDataSource,
+                        recordsState = recordsState,
+                        onEvent = recordsViewModel::onEvent,
                         focusRequester = focusRequester,
                         focusManager = focusManager,
                         interactionSource = interactionSource,
@@ -84,13 +133,16 @@ fun App(
                 }
 
                 Screen.Workout -> {
+
                     WorkoutScreen(
-                        dataSource = appModule.exerciseAppDataSource,
+                        workoutState = workoutState,
+                        onEvent = workoutViewModel::onEvent,
                         focusRequester = focusRequester,
                         focusManager = focusManager,
                         interactionSource = interactionSource,
                         navController = navController
                     )
+
                 }
             }
         }
