7fa19ec366ea3d27e06c1f37466e078c401b146a
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-04 11:19:19 -0700

    Update WorkoutState.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutState.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/WorkoutState.kt
@@ -9,12 +9,6 @@ data class WorkoutState(
 
     val routine: ExerciseRoutine = ExerciseRoutine(),
 
-    val exerciseQueue: List<ExerciseRecord> = listOf(),
-
-    val completedExercises: List<ExerciseRecord> = listOf(),
-
-    val exerciseMap: Map<String, List<ExerciseRecord>> = mapOf(),
-
     val exerciseList: List<ExerciseDefinition> = listOf(),
 
     val recordsList: List<ExerciseRecord> = listOf(),
