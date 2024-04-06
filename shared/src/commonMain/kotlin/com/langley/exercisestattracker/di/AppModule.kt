62e9165941241c1e2138340c03f458be59e006f6
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-05 21:00:42 -0700

    Update AppModule.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/di/AppModule.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/di/AppModule.kt
@@ -1,8 +1,13 @@
 package com.langley.exercisestattracker.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect class AppModule {
     val exerciseAppDataSource: ExerciseAppDataSource
+
+    val preferencesDataStore: DataStore<Preferences>
+
 }
