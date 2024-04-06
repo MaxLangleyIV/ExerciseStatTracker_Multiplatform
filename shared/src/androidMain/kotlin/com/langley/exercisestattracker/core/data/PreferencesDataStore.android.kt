78f66a53bdaba4ea30e8ee683a7428895bda652b
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-06 13:21:31 -0700

    Revert "Reapply "Create PreferencesDataStore.android.kt""
    This reverts commit 7207ad17c0f8eb069df8851f592fd341e7234267.

--- shared/src/androidMain/kotlin/com/langley/exercisestattracker/core/data/PreferencesDataStore.android.kt
+++
@@ -1,9 +0,0 @@
-package com.langley.exercisestattracker.core.data
-
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-
-fun getDataStore(context: Context): DataStore<Preferences> = getDataStore(
-    producePath = { context.filesDir.resolve(dataStoreFileName).absolutePath }
-)
