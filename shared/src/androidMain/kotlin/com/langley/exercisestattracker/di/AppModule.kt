1726abf8c41d95a38dfc4546db58e362fcdb344e
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-06 13:14:59 -0700

    Revert "Update AppModule.kt"
    This reverts commit b817964c29d9f5b680ff9bf26392440dc8758c3d.

--- shared/src/androidMain/kotlin/com/langley/exercisestattracker/di/AppModule.kt
+++ shared/src/androidMain/kotlin/com/langley/exercisestattracker/di/AppModule.kt
@@ -1,13 +1,10 @@
 package com.langley.exercisestattracker.di
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import com.langley.exercisestattracker.core.data.DatabaseDriverFactory
-import com.langley.exercisestattracker.core.data.SqlDelightExerciseAppDataSource
-import com.langley.exercisestattracker.core.data.getDataStore
-import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
 import com.langley.exercisestattracker.database.ExerciseStatTrackerDatabase
+import com.langley.exercisestattracker.core.data.SqlDelightExerciseAppDataSource
+import com.langley.exercisestattracker.core.domain.ExerciseAppDataSource
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class AppModule(
@@ -19,9 +16,5 @@ actual class AppModule(
                 driver = DatabaseDriverFactory(context).createDriver()
             )
         )
-    }
-
-    actual val preferencesDataStore: DataStore<Preferences> by lazy {
-        getDataStore(context)
     }
 }
