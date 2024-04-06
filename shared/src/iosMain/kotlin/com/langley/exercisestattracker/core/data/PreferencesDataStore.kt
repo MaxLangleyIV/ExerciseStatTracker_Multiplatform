af76694ccd7c78308f2e1fa00a58c48d341930b0
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-05 21:00:47 -0700

    Create PreferencesDataStore.kt

---
+++ shared/src/iosMain/kotlin/com/langley/exercisestattracker/core/data/PreferencesDataStore.kt
@@ -0,0 +1,17 @@
+package com.langley.exercisestattracker.core.data
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+
+fun getDataStore(): DataStore<Preferences> = getDataStore(
+    producePath = {
+        val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null,
+        )
+        requireNotNull(documentDirectory).path + "/$dataStoreFileName"
+    }
+)
