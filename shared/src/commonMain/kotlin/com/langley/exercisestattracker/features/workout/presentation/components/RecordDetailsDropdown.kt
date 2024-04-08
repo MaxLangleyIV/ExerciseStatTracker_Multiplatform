98d2cb2e3ac84f8cb8859121379626133fdd938d
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-08 15:55:12 -0700

    Create RecordDetailsDropdown.kt

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/RecordDetailsDropdown.kt
@@ -0,0 +1,70 @@
+package com.langley.exercisestattracker.features.workout.presentation.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.unit.dp
+import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
+
+@Composable
+fun RecordDetailsDropdown(
+    modifier: Modifier = Modifier.fillMaxWidth(),
+    visible: Boolean = false,
+    exercise: ExerciseDefinition = ExerciseDefinition(),
+    set: ExerciseRecord = ExerciseRecord(),
+    setNumber: Int = 0,
+
+    ){
+    AnimatedVisibility(
+        visible = visible,
+        enter = slideInVertically(
+            // Start the slide from 40 (pixels) above where the content is supposed to go, to
+            // produce a parallax effect
+            initialOffsetY = { -40 }
+        ) + expandVertically(
+            expandFrom = Alignment.Top
+        ) + scaleIn(
+            // Animate scale from 0f to 1f using the top center as the pivot point.
+            transformOrigin = TransformOrigin(0.5f, 0f)
+        ) + fadeIn(initialAlpha = 0.3f),
+        exit = slideOutVertically() + shrinkVertically() + fadeOut() + scaleOut(targetScale = 1.2f)
+    ) {
+
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete ${set.exerciseName} Set $setNumber."
+                )
+            }
+        }
+
+    }
+
+}
