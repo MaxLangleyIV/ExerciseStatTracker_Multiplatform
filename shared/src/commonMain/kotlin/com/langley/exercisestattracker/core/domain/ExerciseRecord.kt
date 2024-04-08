32e9923000eec5765e714835d462f6fe004d9608
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-08 15:55:03 -0700

    Update ExerciseRecord.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseRecord.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/domain/ExerciseRecord.kt
@@ -1,5 +1,8 @@
 package com.langley.exercisestattracker.core.domain
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ExerciseRecord(
     val exerciseRecordId: Long? = null,
     val dateCompleted: Long = 0,
