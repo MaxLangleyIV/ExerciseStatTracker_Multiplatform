f0ccc2b4825ad84e641704138aa40356565c7226
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-08 15:55:09 -0700

    Create AlertDialog.kt

---
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/presentation/composables/AlertDialog.kt
@@ -0,0 +1,51 @@
+package com.langley.exercisestattracker.core.presentation.composables
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun AlertDialogPopUp(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+    contentDescription: String = "Alert Dialog Pop Up"
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = contentDescription)
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
