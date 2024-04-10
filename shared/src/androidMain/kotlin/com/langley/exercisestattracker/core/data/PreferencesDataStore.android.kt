23d590eb5de50d1a2aed41e86b0ee9d3d7ae94df
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-10 15:14:40 -0700

    Update PreferencesDataStore.android.kt

--- shared/src/androidMain/kotlin/com/langley/exercisestattracker/core/data/PreferencesDataStore.android.kt
+++ shared/src/androidMain/kotlin/com/langley/exercisestattracker/core/data/PreferencesDataStore.android.kt
@@ -1,11 +1,8 @@
 package com.langley.exercisestattracker.core.data
 
 import android.content.Context
-import androidx.datastore.core.DataMigration
 import androidx.datastore.core.DataStore
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.Preferences
-import kotlinx.coroutines.CoroutineScope
 import java.io.File
 
 actual fun createDataStorePreferences(
