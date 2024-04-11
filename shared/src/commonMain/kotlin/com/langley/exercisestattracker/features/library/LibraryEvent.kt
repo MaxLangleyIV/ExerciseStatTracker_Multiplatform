7a2f90f99d1ef6d2de2d6990fa4c86e6553a0e60
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-11 13:51:34 -0700

    Update LibraryEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/LibraryEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/LibraryEvent.kt
@@ -1,11 +1,15 @@
 package com.langley.exercisestattracker.features.library
 
 import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRoutine
+import com.langley.exercisestattracker.core.domain.ExerciseSchedule
 
 sealed interface LibraryEvent {
 
     // Main Library View Events
     data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition): LibraryEvent
+    data class RoutineSelected(val routine: ExerciseRoutine) : LibraryEvent
+    data class ScheduleSelected(val schedule: ExerciseSchedule) : LibraryEvent
     data class OnSearchStringChanged(val value: String) : LibraryEvent
     data class SetCurrentFilterType(val filterType: ExerciseLibraryFilterType): LibraryEvent
     data class UpdateSelectedDefinition(val definition: ExerciseDefinition): LibraryEvent
@@ -27,8 +31,8 @@ sealed interface LibraryEvent {
 
 
     // For initializing dummy data.
-    data class SaveDefinition(val exerciseDefinition: ExerciseDefinition): LibraryEvent
-
-
+    data class SaveDefinition(val definition: ExerciseDefinition): LibraryEvent
+    data class SaveRoutine(val routine: ExerciseRoutine): LibraryEvent
+    data class SaveSchedule(val schedule: ExerciseSchedule): LibraryEvent
 
 }
