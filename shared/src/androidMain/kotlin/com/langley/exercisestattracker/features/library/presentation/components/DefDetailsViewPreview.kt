2edc3cab0b78218ea797fbb085615a88e3755bf0
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-13 12:54:46 -0700

    Update DefDetailsViewPreview.kt

--- shared/src/androidMain/kotlin/com/langley/exercisestattracker/features/library/presentation/components/DefDetailsViewPreview.kt
+++ shared/src/androidMain/kotlin/com/langley/exercisestattracker/features/library/presentation/components/DefDetailsViewPreview.kt
@@ -3,6 +3,7 @@ package com.langley.exercisestattracker.features.library.presentation.components
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.langley.exercisestattracker.core.data.dummyData.ExerciseDefinitionDummyData
+import com.langley.exercisestattracker.features.library.presentation.components.exercises.DefinitionDetailsView
 
 @Preview
 @Composable
