a4f56288bd9f85db5be5754eb3f2d9cd28fe2516
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-14 16:11:28 -0700

    Update ScheduleBuilderEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/schedules/ScheduleBuilderEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/schedules/ScheduleBuilderEvent.kt
@@ -1,7 +1,10 @@
 package com.langley.exercisestattracker.features.library.schedules
 
+import com.langley.exercisestattracker.core.domain.ExerciseRoutine
+
 sealed interface ScheduleBuilderEvent {
     data class OnSearchStringChanged(val value: String) : ScheduleBuilderEvent
+    data class AddToListOfRoutines(val routines: List<ExerciseRoutine>) : ScheduleBuilderEvent
 
     data object CloseExerciseSelector : ScheduleBuilderEvent
 }
