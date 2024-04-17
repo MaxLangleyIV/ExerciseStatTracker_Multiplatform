3a9daf218f98f1bc1b99b7a72123bf39d83a9056
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-17 15:34:19 -0700

    Update LibraryEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/LibraryEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/LibraryEvent.kt
@@ -7,28 +7,31 @@ import com.langley.exercisestattracker.core.domain.ExerciseSchedule
 sealed interface LibraryEvent {
 
     // Main Library View Events
-    data class DefinitionSelected(val exerciseDefinition: ExerciseDefinition): LibraryEvent
+    data class ExerciseSelected(val exercise: ExerciseDefinition): LibraryEvent
     data class RoutineSelected(val routine: ExerciseRoutine) : LibraryEvent
     data class ScheduleSelected(val schedule: ExerciseSchedule) : LibraryEvent
     data class OnSearchStringChanged(val value: String) : LibraryEvent
     data class SetCurrentFilterType(val filterType: ExerciseLibraryFilterType?): LibraryEvent
-    data class UpdateSelectedDefinition(val definition: ExerciseDefinition): LibraryEvent
+    data class UpdateSelectedExercise(val exercise: ExerciseDefinition): LibraryEvent
     data object ClearFilterType : LibraryEvent
     data object ToggleIsSearchDropdownOpen: LibraryEvent
-    data object AddNewDefClicked : LibraryEvent
+    data object AddNewExercise : LibraryEvent
+    data object AddNewRoutine : LibraryEvent
     data object CloseAddDefClicked : LibraryEvent
-    data object ClearSelectedDef : LibraryEvent
-    data object SelectDefinitionsTab : LibraryEvent
+    data object ClearSelectedExercise : LibraryEvent
+    data object ClearSelectedRoutine : LibraryEvent
+    data object SelectExercisesTab : LibraryEvent
     data object SelectRoutinesTab : LibraryEvent
     data object SelectSchedulesTab : LibraryEvent
 
 
 
+
     // Details View Events
-    data class EditDefinition(val exerciseDefinition: ExerciseDefinition) : LibraryEvent
+    data class EditExercise(val exerciseDefinition: ExerciseDefinition) : LibraryEvent
     data object EditRoutine : LibraryEvent
     data object EditSchedule : LibraryEvent
-    data class ToggleFavoriteDef(val definition: ExerciseDefinition): LibraryEvent
+    data class ToggleFavoriteExercise(val exercise: ExerciseDefinition): LibraryEvent
     data class ToggleFavoriteRoutine(val routine: ExerciseRoutine): LibraryEvent
     data class ToggleFavoriteSchedule(val schedule: ExerciseSchedule): LibraryEvent
     data object CloseDetailsView : LibraryEvent
@@ -38,7 +41,7 @@ sealed interface LibraryEvent {
 
 
     // For initializing dummy data.
-    data class SaveDefinition(val definition: ExerciseDefinition): LibraryEvent
+    data class SaveExercise(val exercise: ExerciseDefinition): LibraryEvent
     data class SaveRoutine(val routine: ExerciseRoutine): LibraryEvent
     data class SaveSchedule(val schedule: ExerciseSchedule): LibraryEvent
 
