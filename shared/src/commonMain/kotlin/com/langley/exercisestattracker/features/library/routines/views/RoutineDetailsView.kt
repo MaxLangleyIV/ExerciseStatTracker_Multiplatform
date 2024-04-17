7e744234bed8cb1b5d46cd3a11c19125771923e7
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-17 15:34:29 -0700

    Update RoutineDetailsView.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/routines/views/RoutineDetailsView.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/routines/views/RoutineDetailsView.kt
@@ -1,5 +1,14 @@
 package com.langley.exercisestattracker.features.library.routines.views
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,7 +19,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarOutline
@@ -28,141 +39,152 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.langley.exercisestattracker.core.domain.ExerciseRoutine
-import com.langley.exercisestattracker.core.domain.ExerciseSchedule
 import com.langley.exercisestattracker.core.presentation.composables.BasicBottomSheet
 import com.langley.exercisestattracker.features.library.LibraryEvent
 
 @Composable
 fun RoutineDetailsView(
     modifier: Modifier = Modifier,
-    isVisible: Boolean = false,
+    visible: Boolean = false,
     routine: ExerciseRoutine = ExerciseRoutine(),
     libraryOnEvent: (LibraryEvent) -> Unit
 ){
-
-    BasicBottomSheet(
-        visible = isVisible,
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
-    )
-    {
-
-        // Top Row
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn(
+            animationSpec = tween(durationMillis = 100),
+        ),
+        exit = fadeOut(
+            animationSpec = tween(durationMillis = 100),
         )
-        {
-            IconButton(
-                onClick = {
-                    libraryOnEvent(LibraryEvent.CloseDetailsView)
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.Close,
-                    contentDescription = "Close"
-                )
-            }
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(8.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-            IconButton(
-                onClick = {
-                    libraryOnEvent(LibraryEvent.ToggleFavoriteRoutine(routine))
-                }
-            ) {
-                Icon(
-
-                    imageVector =
-                    if (
-                        routine.isFavorite
-                    ) {
-                        Icons.Filled.Star
-                    }
-                    else { Icons.Filled.StarOutline },
-
-                    contentDescription = "Favorite"
-                )
-
-            }
-
-            Text(
-                text = "Edit",
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(16.dp)
-                    .clickable {
-                               libraryOnEvent(LibraryEvent.EditRoutine)
-                    },
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
+            // Top Row
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             )
-        }
-
-        // Title Section
-        Column (
-            modifier = Modifier.fillMaxSize()
-                .background(MaterialTheme.colorScheme.surface),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        )
-        {
-            Spacer(Modifier.height(16.dp))
-
-
-            Column()
             {
+                IconButton(
+                    onClick = {
+                        libraryOnEvent(LibraryEvent.CloseDetailsView)
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Close,
+                        contentDescription = "Close"
+                    )
+                }
+
+                IconButton(
+                    onClick = {
+                        libraryOnEvent(LibraryEvent.ToggleFavoriteRoutine(routine))
+                    }
+                ) {
+                    Icon(
+
+                        imageVector =
+                        if (
+                            routine.isFavorite
+                        ) {
+                            Icons.Filled.Star
+                        }
+                        else { Icons.Filled.StarOutline },
+
+                        contentDescription = "Favorite"
+                    )
+
+                }
 
                 Text(
-                    text = routine.routineName,
+                    text = "Edit",
                     textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.padding(16.dp)
+                        .clickable {
+                            libraryOnEvent(LibraryEvent.EditRoutine)
+                        },
                     fontWeight = FontWeight.Bold,
-                    fontSize = 36.sp,
-                    lineHeight = 40.sp,
-                    color = MaterialTheme.colorScheme.onSurface
+                    fontSize = 20.sp,
                 )
-                Spacer(Modifier.height(8.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(8.dp)
-                        .clip(
-                            RoundedCornerShape(16.dp)
-                        )
-                        .background(MaterialTheme.colorScheme.tertiaryContainer)
-
-                ){}
             }
+
+            // Title Section
+            Column (
+                modifier = Modifier.fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surface),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            )
+            {
+                Spacer(Modifier.height(16.dp))
+
+
+                Column()
+                {
+
+                    Text(
+                        text = routine.routineName,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 36.sp,
+                        lineHeight = 40.sp,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(8.dp)
+                            .clip(
+                                RoundedCornerShape(16.dp)
+                            )
+                            .background(MaterialTheme.colorScheme.tertiaryContainer)
+
+                    ){}
+                }
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = routine.description,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = routine.exerciseCSV,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text(
+                text = routine.repsCSV,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp
+            )
+
         }
-
-        Spacer(Modifier.height(16.dp))
-
-        Text(
-            text = routine.description,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            fontWeight = FontWeight.Normal,
-            fontSize = 20.sp
-        )
-
-        Spacer(Modifier.height(16.dp))
-
-        Text(
-            text = routine.exerciseCSV,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            fontWeight = FontWeight.Normal,
-            fontSize = 20.sp
-        )
-
-        Spacer(Modifier.height(16.dp))
-
-        Text(
-            text = routine.repsCSV,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            fontWeight = FontWeight.Normal,
-            fontSize = 20.sp
-        )
-
     }
 
 }
