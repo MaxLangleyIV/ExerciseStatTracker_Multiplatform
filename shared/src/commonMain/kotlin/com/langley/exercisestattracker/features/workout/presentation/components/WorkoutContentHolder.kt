543049ebfbffccf1b2081770e738eb3e8c357f67
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-17 19:04:18 -0700

    Update WorkoutContentHolder.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WorkoutContentHolder.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/workout/presentation/components/WorkoutContentHolder.kt
@@ -8,31 +8,42 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
 import com.langley.exercisestattracker.features.workout.WorkoutEvent
 import com.langley.exercisestattracker.features.workout.WorkoutState
 
 @Composable
 fun WorkoutContentHolder(
     modifier: Modifier = Modifier,
-    workoutState: WorkoutState,
-    onEvent: (WorkoutEvent) -> Unit = {}
+//    workoutState: WorkoutState,
+    exercises: List<ExerciseDefinition> = emptyList(),
+    records: List<ExerciseRecord> = emptyList(),
+//    onEvent: (WorkoutEvent) -> Unit = {},
+    openExerciseSelector: () -> Unit = {},
+    openRoutineSelector: () -> Unit = {},
+    updateRepsFromString: (index: Int, string: String) -> Unit = { _, _ -> },
+    updateWeightFromString: (index: Int, string: String) -> Unit = { _, _ -> },
+    markSetComplete: (index: Int, set: ExerciseRecord) -> Unit = { _, _ -> },
+    markSetIncomplete: (index: Int, set: ExerciseRecord) -> Unit = { _, _ -> },
+    addToListOfRecords: (list: List<ExerciseRecord>) -> Unit = {},
+    routineBuilderMode: Boolean = false
 ){
 
     // Workout Content
     Column(
         modifier = modifier.verticalScroll(rememberScrollState()),
-
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
 
         // Empty Workout View
-        if (workoutState.exerciseList.isEmpty()){
+        if (exercises.isEmpty()){
 
             EmptyWorkoutContent(
                 modifier = Modifier,
-                workoutState = workoutState,
-                onEvent = onEvent
+                openExerciseSelector = openExerciseSelector,
+                openRoutineSelector = openRoutineSelector
             )
 
         }
@@ -42,9 +53,15 @@ fun WorkoutContentHolder(
 
             WorkoutContent(
                 modifier = Modifier.fillMaxSize(),
-                exercises = workoutState.exerciseList,
-                records = workoutState.recordsList,
-                onEvent = onEvent
+                exercises = exercises,
+                records = records,
+                openExerciseSelector = openExerciseSelector,
+                updateRepsFromString = updateRepsFromString,
+                updateWeightFromString = updateWeightFromString,
+                markSetComplete = markSetComplete,
+                markSetIncomplete = markSetIncomplete,
+                addToListOfRecords = addToListOfRecords
+
             )
 
         }
