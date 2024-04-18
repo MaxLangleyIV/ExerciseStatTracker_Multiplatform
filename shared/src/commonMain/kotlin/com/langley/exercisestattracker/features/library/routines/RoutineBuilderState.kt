35a141c526650a36ca44e50268a1158f8e32aa63
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-18 11:00:05 -0700

    Update RoutineBuilderState.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/routines/RoutineBuilderState.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/routines/RoutineBuilderState.kt
@@ -1,5 +1,7 @@
 package com.langley.exercisestattracker.features.library.routines
 
+import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
 import com.langley.exercisestattracker.core.domain.ExerciseRoutine
 import kotlinx.serialization.Serializable
 
@@ -8,6 +10,15 @@ data class RoutineBuilderState(
     val initialized: Boolean = false,
 
     val routine: ExerciseRoutine = ExerciseRoutine(),
+
+    val exerciseList: List<ExerciseDefinition> = emptyList(),
+    val recordList: List<ExerciseRecord> = emptyList(),
+
+    val exerciseIdString: String = "",
+    val repsString: String = "",
+
+    // UI
+    val isSelectorOpen: Boolean = false,
 
     //Input validation errors state.
     val nameError: String? = null,
