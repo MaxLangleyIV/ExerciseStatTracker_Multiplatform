ce34a23ad3ebd2b0c8b2785cf32eddd93cbd3fab
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-04-19 20:56:52 -0700

    Update RoutineBuilderEvent.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/routines/RoutineBuilderEvent.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/library/routines/RoutineBuilderEvent.kt
@@ -1,11 +1,13 @@
 package com.langley.exercisestattracker.features.library.routines
 
 import com.langley.exercisestattracker.core.domain.ExerciseDefinition
+import com.langley.exercisestattracker.core.domain.ExerciseRecord
 import com.langley.exercisestattracker.core.domain.ExerciseRoutine
 
 sealed interface RoutineBuilderEvent {
     data class OnSearchStringChanged(val value: String) : RoutineBuilderEvent
     data class AddToListOfExercises(val exercises: List<ExerciseDefinition>) : RoutineBuilderEvent
+    data class AddRecords(val records: List<ExerciseRecord>) : RoutineBuilderEvent
     data class RemoveExercise(val index: Int) : RoutineBuilderEvent
     data object OpenSelector : RoutineBuilderEvent
     data object CloseSelector : RoutineBuilderEvent
