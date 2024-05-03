32eedcde2883c2d74338251100805b502bd3b560
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-05-03 16:28:51 -0700

    Update SerializationUtils.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/data/SerializationUtils.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/core/data/SerializationUtils.kt
@@ -1,15 +1,25 @@
 package com.langley.exercisestattracker.core.data
 
 import com.langley.exercisestattracker.features.workout.WorkoutState
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 
 fun getWorkoutStateFromString(string: String): WorkoutState {
     return if (string.isBlank()){
-        println("RETURNING AS BLANK")
         WorkoutState()
     } else {
-        val workout = Json.decodeFromString<WorkoutState>(string)
-        println("RETURNING AS: $workout")
-        workout
+        try {
+            Json.decodeFromString<WorkoutState>(string)
+        }
+        catch (error: SerializationException, ){
+            println(error)
+
+            return WorkoutState()
+        }
+        catch (error: IllegalArgumentException){
+            println(error)
+
+            return WorkoutState()
+        }
     }
 }
