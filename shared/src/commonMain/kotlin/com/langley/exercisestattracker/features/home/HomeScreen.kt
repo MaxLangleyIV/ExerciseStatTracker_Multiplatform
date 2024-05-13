e1222a4bc131cbac17a5b265ed90121b03a0fda7
MaxLangleyIV <32246043+MaxLangleyIV@users.noreply.github.com>
2024-05-13 15:00:12 -0700

    Update HomeScreen.kt

--- shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/langley/exercisestattracker/features/home/HomeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,29 +49,20 @@ fun HomeScreen(
                 verticalArrangement = Arrangement.Center,
             ) {
 
+                Spacer(modifier = Modifier.height(8.dp))
+
                 // Date and User Profile
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(0.3F)
+                        .weight(0.25F)
                         .clip(RoundedCornerShape(18.dp))
                         .background(MaterialTheme.colorScheme.surfaceVariant),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center,
-                ){}
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                // Start Workout
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(0.3F)
-                        .clip(RoundedCornerShape(18.dp))
-                        .background(MaterialTheme.colorScheme.surfaceVariant),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                ){}
+                ){
+                    Text(text = "Date and User Details")
+                }
 
                 Spacer(modifier = Modifier.height(8.dp))
 
@@ -78,12 +70,31 @@ fun HomeScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(0.3F)
+                        .weight(0.25F)
                         .clip(RoundedCornerShape(18.dp))
                         .background(MaterialTheme.colorScheme.surfaceVariant),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center,
-                ){}
+                ){
+                    Text(text = "Weekly Stats")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // Start Workout
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(0.25F)
+                        .clip(RoundedCornerShape(18.dp))
+                        .background(MaterialTheme.colorScheme.surfaceVariant),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ){
+                    Text(text = "Start Workout")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
 
 
                 
